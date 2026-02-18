7e6abe4a77090b0a4c014d742c217538a3869ed5
kkontras <konstantinoskontras@gmail.com>
2026-02-18 06:10:05 +0100

    init

--- src/synib/utils/configuration/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import process_config, process_config_default, setup_logger
-from .dirs import create_dirs
-
-__all__ = ["process_config", "process_config_default", "setup_logger", "create_dirs"]
--- src/synib/utils/optimization/__init__.py
+++
@@ -1,8 +0,0 @@
-from .gs_plugin import GSPlugin
-from .min_norm_solver import MinNormSolver
-try:
-    from .normalized_adamw import NormalizedAdamW
-except Exception:
-    NormalizedAdamW = None
-
-__all__ = ["GSPlugin", "MinNormSolver", "NormalizedAdamW"]
