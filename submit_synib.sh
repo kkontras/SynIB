68f149be9ab662f214f4994c3d74f3e39d9bbfe0
kkontras <konstantinoskontras@gmail.com>
2026-03-21 06:59:52 +0100

    mib

--- submit_synib.sh
+++ submit_synib.sh
@@ -5,6 +5,7 @@
 
 SLURM_OPTS="--gres=gpu:1 --time=60:00:00 -p pi_ppliang --nodelist=node2500 -c 15 --mem=80G"
 WORKDIR=$(pwd)
+CONDA_INIT="source ~/miniconda3/bin/activate synergy_new"
 
 for script in train_mustard_synib_lora.sh train_mustard_synibu_lora.sh; do
     while IFS= read -r line; do
@@ -12,9 +13,9 @@ for script in train_mustard_synib_lora.sh train_mustard_synibu_lora.sh; do
         # SLURM assigns the GPU; fix device index to 0
         cmd=$(echo "$line" | sed 's/CUDA_VISIBLE_DEVICES=[0-9]*/CUDA_VISIBLE_DEVICES=0/')
         if [[ "${DRY_RUN:-0}" == "1" ]]; then
-            echo "sbatch $SLURM_OPTS --wrap=\"cd $WORKDIR && $cmd\""
+            echo "sbatch $SLURM_OPTS --wrap=\"cd $WORKDIR && $CONDA_INIT && $cmd\""
         else
-            sbatch $SLURM_OPTS --wrap="cd $WORKDIR && $cmd"
+            sbatch $SLURM_OPTS --wrap="cd $WORKDIR && $CONDA_INIT && $cmd"
         fi
     done < "$script"
 done
